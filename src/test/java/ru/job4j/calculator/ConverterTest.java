05fee6d2f219c8f79d58e7c2291ec7e63515f2c1
masha <musha_skorpion@mail.ru>
2024-07-21 18:57:52 +0500

    1.Модульные тесты. [#159435]

---
+++ src/test/java/ru/job4j/calculator/ConverterTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.calculator;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class ConverterTest {
+
+    @org.junit.jupiter.api.Test
+    void whenConvert140RblThen2Euro() {
+        float input = 140;
+        float expected = 2;
+        float output = Converter.rubleToEuro(input);
+        float value = 0.0001f;
+        assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+
+    @org.junit.jupiter.api.Test
+    void whenConvert180RblThen3DOllar() {
+        float input = 180;
+        float expected = 3;
+        float output = Converter.rubleToDollar(input);
+        float value = 0.0001f;
+        assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+}
