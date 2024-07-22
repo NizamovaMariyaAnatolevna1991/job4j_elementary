a07d43673620171e3010d3d8bf21995f1c361ebc
masha <musha_skorpion@mail.ru>
2024-07-23 00:04:26 +0500

    Арифметические операции. [#185 #505616]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -16,6 +16,18 @@ public class Calculator {
         plus(10, 11);
         minus(7, 3);
         minus(2, 6);
+        int one = 1;
+        int two = 2;
+        int six = 6;
+        int four = 4;
+        int five = 5;
+        int onePlusTwo = one + two;
+        int sixDivTwo = six / two;
+        int fiveMinusTwo = five - two;
+        int fourTimeTwo = four * two;
+        System.out.println(onePlusTwo);
+        System.out.println(sixDivTwo);
+        System.out.println(fiveMinusTwo);
+        System.out.println(fourTimeTwo);
     }
-
 }
