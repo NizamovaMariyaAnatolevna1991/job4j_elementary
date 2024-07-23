2ea436b099784d06856e03ae5768fe814f7be5d8
masha <musha_skorpion@mail.ru>
2024-07-24 00:51:05 +0500

    GPT. Нейросети спешат на помощь. [#505156]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -1,20 +1,20 @@
 package ru.job4j.calculator;
 
 public class Fit {
-    public static double manWeight(short height) {
+    public static double calculateManWeight(double height) {
          double result = (height - 100) * 1.15;
          return result;
     }
 
-    public static double womanWeight(short height) {
+    public static double calculateWomanWeight(double height) {
         double result = (height - 110) * 1.15;
         return result;
     }
 
     public static void main(String[] args) {
-        short height = 180;
-        double man = Fit.manWeight(height);
-        double woman = Fit.womanWeight(height);
+        double height = 180;
+        double man = Fit.calculateManWeight(height);
+        double woman = Fit.calculateWomanWeight(height);
         System.out.println("Woman 187 is " + woman);
         System.out.println("Man 187 is " + man);
     }
