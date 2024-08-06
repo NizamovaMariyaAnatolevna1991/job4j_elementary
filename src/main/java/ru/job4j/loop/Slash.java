af286054c6050901b13707069334369a837b1abe
masha <musha_skorpion@mail.ru>
2024-08-07 01:36:55 +0500

    5.9. Крест в псевдографике. [#177182]

--- src/main/java/ru/job4j/loop/Slash.java
+++ src/main/java/ru/job4j/loop/Slash.java
@@ -4,8 +4,8 @@ public class Slash {
     public static void draw(int size) {
         for (int row = 0; row < size; row++) {
             for (int cell = 0; cell < size; cell++) {
-                boolean left = cell == row;  /* добавить условие для левой диагонали - с левого верхнего угла в правый нижний*/
-                boolean right = cell + row == size - 1; /* добавить условие для правой диагонали - из левого нижнего в правый верхний*/
+                boolean left = cell == row;
+                boolean right = cell + row == size - 1;
                 if (left) {
                     System.out.print("0");
                 } else  if (right) {
