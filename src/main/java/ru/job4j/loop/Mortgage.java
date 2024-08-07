1cc84ad598313dc655a1fac26098920b51634772
masha <musha_skorpion@mail.ru>
2024-08-08 01:55:31 +0500

    5.7. Ипотека [#156315 #507859]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -2,11 +2,10 @@ package ru.job4j.loop;
 
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
-        int year = 1;
-        double balance = amount + amount * percent / 100 - salary;
-        while (balance > 0) {
-            balance = balance * percent / 100 + (balance - salary);
-            year += 1;
+        int year = 0;
+        while (amount > 0) {
+            amount = amount + amount * percent / 100 - salary;
+            year++;
         }
         return year;
     }
