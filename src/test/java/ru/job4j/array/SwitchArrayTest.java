695ae56bb11d752be1faab2f17e969bfc5c674bf
masha <musha_skorpion@mail.ru>
2024-08-11 00:19:25 +0500

    6.2.2. Переставить элементы массива [#226795]

--- src/test/java/ru/job4j/array/SwitchArrayTest.java
+++ src/test/java/ru/job4j/array/SwitchArrayTest.java
@@ -35,4 +35,25 @@ class SwitchArrayTest {
         int[] expected = {1};
         assertThat(result).containsExactly(expected);
     }
+
+    @Test
+    void whenSwap0to3() {
+        int[] input = {1, 2, 3, 4};
+        int source = 0;
+        int destination = input.length - 1;
+        int[] result = SwitchArray.swap(input, source, destination);
+        int[] expected = {4, 2, 3, 1};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    void whenSwap4to5() {
+        int[] input = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int source = 4;
+        int destination = 5;
+        int[] result = SwitchArray.swap(input, source, destination);
+        int[] expected = {1, 2, 3, 4, 6, 5, 7, 8, 9, 10};
+        assertThat(result).containsExactly(expected);
+    }
+
 }
