1429ce4551927c9f7088393d65e13d90bc93523a
masha <musha_skorpion@mail.ru>
2024-08-28 01:19:49 +0500

    1.8. Класс использующий сам себя [#167388 #510927]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -7,45 +7,37 @@ class PointTest {
 
     @Test
     void when00to20then() {
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double output = Point.distance(x1, y1, x2, y2);
+        double output = a.distance(b);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void when3525to3530then5() {
+        Point a = new Point(35, 25);
+        Point b = new Point(35, 30);
         double expected = 5;
-        int x1 = 35;
-        int y1 = 25;
-        int x2 = 35;
-        int y2 = 30;
-        double output = Point.distance(x1, y1, x2, y2);
+        double output = a.distance(b);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void whenMinus56to2Minus59then3() {
+        Point a = new Point(-5, 6);
+        Point b = new Point(-5, 9);
         double expected = 3;
-        int x1 = -5;
-        int y1 = 6;
-        int x2 = -5;
-        int y2 = 9;
-        double output = Point.distance(x1, y1, x2, y2);
+        double output = a.distance(b);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void when619to719then1() {
+        Point a = new Point(6, 19);
+        Point b = new Point(7, 19);
         double expected = 1;
-        int x1 = 6;
-        int y1 = 19;
-        int x2 = 7;
-        int y2 = 19;
-        double output = Point.distance(x1, y1, x2, y2);
+        double output = a.distance(b);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 }
