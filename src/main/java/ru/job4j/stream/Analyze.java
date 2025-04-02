98e4c439aa5226438cb6089c1b1b86406e2251ae
masha <musha_skorpion@mail.ru>
2025-04-03 00:12:39 +0500

    Аттестация [#49777]

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -58,7 +58,6 @@ public class Analyze {
                 .flatMap(pupil -> pupil.subjects().stream())
                 .collect(Collectors.groupingBy(
                         Subject::name,
-                        LinkedHashMap::new,
                         Collectors.summingDouble(Subject::score)
                 ))
                 .entrySet()
